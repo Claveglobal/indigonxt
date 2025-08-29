b55eabb10e443226e40e05e5db902ac1da924352
Francisco Lo Giudice <francisco@arjuna.ai>
2025-08-29 10:40:25 -0300

    cambios en footer

--- footer.php
+++ footer.php
@@ -1,21 +1,19 @@
-
-  <!-- 12) Footer -->
-  <footer class="bg-gray-800 text-white py-6">
-    <div class="container w-11/12 mx-auto flex flex-col sm:flex-row py-20 sm:justify-between">
-      <div class="flex sm:items-center sm:justify-center sm:justify-start mb-8 sm:mb-0">
-        <img src="images/logo-footer.svg" class="w-40 transition-transform duration-300 hover:scale-105">
-      </div>
-      <div class="w-full sm:w-7/12 flex flex-col sm:flex-row sm:justify-between text-[10px] text-left space-y-8 sm:space-y-0 sm:space-x-8 sm:items-start">
-        <div class="flex flex-col ">DESARROLLO<br><br><img src="images/nordelta.svg" class="w-40"></div>
-        <div class="flex flex-col  ">PROYECTO<br><br><img src="images/msgssv.svg" class="w-32"></div>
-        <div class="flex flex-col ">CONSTRUYE<br><br><img src="images/nxt.svg" class="w-32"></div>
-        <div class="flex flex-col  ">OFICINAS<br><br><span class="text-xs">Av. de los Lagos 205, Officia Nordelta, <br>1º BB1670 Tigre, Nordelta</span></div>
-      </div>
+<!-- 12) Footer -->
+<footer class="bg-gray-800 text-white py-6">
+  <div class="container w-11/12 mx-auto flex flex-col sm:flex-row py-20 sm:justify-between">
+    <div class="flex sm:items-center sm:justify-center sm:justify-start mb-8 sm:mb-0">
+      <img src="images/logo-footer.svg" class="w-40 transition-transform duration-300 hover:scale-105">
     </div>
-    <div class="container w-11/12 mx-auto flex flex-col sm:flex-row pb-20 sm:justify-between">
-      <div class="flex sm:items-center sm:justify-center sm:justify-start mb-8 sm:mb-0"></div>
-      <div class="w-full sm:w-7/12 flex flex-col sm:flex-row sm:justify-between text-[16px] text-left space-y-8 sm:space-y-0 sm:space-x-8 sm:items-start">
-        <p>© 2025 Todos los derechos reservados.</p>
-      </div>
+    <div class="w-full sm:w-7/12 flex flex-col sm:flex-row sm:justify-between text-[10px] text-left space-y-8 sm:space-y-0 sm:space-x-8 sm:items-start">
+      <div class="flex flex-col ">DESARROLLA Y CONSTRUYE<br><br><img src="images/nxt.svg" class="w-32"></div>
+      <div class="flex flex-col  ">PROYECTO<br><br><img src="images/msgssv.svg" class="w-32"></div>
+      <div class="flex flex-col  ">OFICINAS<br><br><span class="text-xs">Av. de los Lagos 205, Officia Nordelta, <br>1º BB1670 Tigre, Nordelta</span></div>
     </div>
-  </footer>
+  </div>
+  <div class="container w-11/12 mx-auto flex flex-col sm:flex-row pb-20 sm:justify-between">
+    <div class="flex sm:items-center sm:justify-center sm:justify-start mb-8 sm:mb-0"></div>
+    <div class="w-full sm:w-7/12 flex flex-col sm:flex-row sm:justify-between text-[16px] text-left space-y-8 sm:space-y-0 sm:space-x-8 sm:items-start">
+      <p>© 2025 Todos los derechos reservados.</p>
+    </div>
+  </div>
+</footer>
